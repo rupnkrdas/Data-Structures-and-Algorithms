2a316bba220530ec25c6877bb45475a79bb0bc95
rupnkrdas <88283289+rupnkrdas@users.noreply.github.com>
2022-07-01 18:08:12 +0530

    updated ReverseLL.java

--- 14-Linked-Lists/code/src/ReverseLL.java
+++ 14-Linked-Lists/code/src/ReverseLL.java
@@ -37,14 +37,3 @@ public class ReverseLL {
         return left;
     }
 }
-
-// Definition for singly-linked list.
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode(int x) {
-        val = x;
-        next = null;
-    }
-}
